08f9ab244a84454b8578dc2a7b677fab097f8be5
Bonjour Arcade <bonjourarcade@gmail.com>
2025-04-25 16:25:36 -0400

    overwrite files without prompting
    Former-commit-id: 9ee25e0951c5c01cb4e170cc3c0d1ac5c29fcade

--- public/setup-mister.sh
+++ public/setup-mister.sh
@@ -3,12 +3,9 @@ set -e
 # Install barcode launcher code
 curl -sSLk https://felx.cc/install-barcode | sh
 
-# Update barcode database
-/media/fat/Scripts/update_barcode_database.sh
-
 # Download game archive and expand locally
 cd /media/fat
 wget -N https://bonjourarcade-f11f7f.gitlab.io/games.zip
-unzip games.zip
+unzip -o games.zip
 
 echo "Setup and update complete! :) Happy gaming."

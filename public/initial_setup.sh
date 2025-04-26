ed3f46b16cb6ffdcd6e52e5e5624f781228ab607
BonjourArcade <bonjourarcade@gmail.com>
2025-04-26 08:53:06 -0400

    update initial_setup.sh

--- public/initial_setup.sh
+++ public/initial_setup.sh
@@ -2,8 +2,8 @@ wget -N -P /media/fat/Scripts https://felx.cc/update_bonjourarcade
 
 # Add missing .sh extension to get recognized in the MiSTer Scripts
 # menu
-mv /media/fat/Scripts/update_bonjourarcade{,.sh}
+mv /media/fat/Scripts/{update_bonjourarcade,\@update_bonjourarcade.sh}
 
 # Launch update_bonjourarcade.sh!
 
-/media/fat/Scripts/update_bonjourarcade.sh
+/media/fat/Scripts/\@update_bonjourarcade.sh

e3e60d68aa16d89f96c972dabe7d7e65a11a3f58
BonjourArcade <bonjourarcade@gmail.com>
2026-02-02 22:19:28 -0500

    Add umami analytics, GDPR compliant.

---
+++ public/assets/js/analytics.js
@@ -0,0 +1,14 @@
+(function () {
+    // Disable on localhost
+    if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
+        console.log('BonjourArcade: Analytics disabled on localhost');
+        return;
+    }
+
+    // BonjourArcade Analytics (Umami)
+    const script = document.createElement('script');
+    script.defer = true;
+    script.src = "https://cloud.umami.is/script.js";
+    script.setAttribute('data-website-id', "660e5f95-7427-4bee-b7a5-4f50fa389e4e");
+    document.head.appendChild(script);
+})();
